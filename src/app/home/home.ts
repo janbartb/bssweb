228683e955287cbfce1ff7687b4b5d0f5aedc794
Jan Bart Biemans <j.biemans4@chello.nl>
2026-02-14 13:23:51 +0100

    home pagina

---
+++ src/app/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    imports: [],
+    templateUrl: './home.html',
+    styleUrl: './home.css',
+})
+export class Home {
+    version: string = '1.0.0';
+}
